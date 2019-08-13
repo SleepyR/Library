c90ad4e64c0fa668e7c03e1356423c9b393bf6dc
Rozi Tunyan <SleepyR>
2019-08-13 15:20:53 +0400

    "Added jsons"

--- lms/User.js
+++ lms/User.js
@@ -1,9 +1,10 @@
 class User{
-    constructor(id,firstName, lastName, phone, email){
+    constructor(id,firstName, lastName, phone, email,role){
         this.id = id;
         this.name = firstName;
         this.surname = lastName;
         this.phoneNumber = phone;
         this.email = email;
+        this.role = role;
     }
 }
