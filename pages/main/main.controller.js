e98fb02ae92ff4a22daac1cfa1d8e53a3e60c180
Rozi Tunyan <SleepyR>
2019-08-16 23:44:25 +0400

    main page buttons profiles

--- pages/main/main.controller.js
+++ pages/main/main.controller.js
@@ -1,5 +1,7 @@
 window.onload = ()=>{
     createBooks();
+    createStaff();
+    document.getElementById("staff").style.display = "none";
     window.lms = new LMS();
     let logOut = document.getElementById("signIn/signOut");
     if(window.lms.authService.isLogined()){
@@ -7,19 +9,82 @@ window.onload = ()=>{
         logOut.href = "gallery.html";
         logOut.onclick = window.lms.authService.logout;
     }
+    showLabel("Books");
 };
-
+function showLabel(label) {
+    let div = document.getElementById("name");
+    div.innerHTML = label;
+    let buttons = document.getElementById("buttons");
+    let button1 = document.createElement("div");
+    let button2 = document.createElement("div");
+    let button3 = document.createElement("div");
+    switch (label) {
+        case "Users":
+            button1.setAttribute("class","button border-radius background-primary text-size-12 text-white text-strong");
+            button1.innerHTML = "ADD USER";
+            button2.setAttribute("class","button border-radius background-primary text-size-12 text-white text-strong");
+            button2.innerHTML = "EDIT USER";
+            button3.setAttribute("class","button border-radius background-primary text-size-12 text-white text-strong");
+            button3.innerHTML = "DELETE USER";
+            buttons.appendChild(button3);
+            buttons.appendChild(button2);
+            buttons.appendChild(button1);
+            break;
+        case "Books":
+            button1.setAttribute("class","button border-radius background-primary text-size-12 text-white text-strong");
+            button1.innerHTML = "ADD BOOK";
+            button2.setAttribute("class","button border-radius background-primary text-size-12 text-white text-strong");
+            button2.innerHTML = "EDIT BOOK";
+            button3.setAttribute("class","button border-radius background-primary text-size-12 text-white text-strong");
+            button3.innerHTML = "DELETE BOOK";
+            buttons.appendChild(button3);
+            buttons.appendChild(button2);
+            buttons.appendChild(button1);
+            break;
+    }
+    document.getElementById("label").appendChild(buttons);
+}
 function showStaff() {
     document.getElementById("container").style.display = "none";
-    let div = document.createElement("div");
-    div.innerHTML = "Welcome To Our Library"
-    document.body.appendChild(div);
-
+    document.getElementById("books").style.display = "none";
+    document.getElementById("staff").style.display = "block";
+    showLabel("Users");
 }
 function showBooks() {
     document.getElementById("container").style.display = "block";
+    document.getElementById("books").style.display = "block";
+    document.getElementById("staff").style.display = "none";
+    showLabel("Books");
+}
+function createStaff() {
+    let users = JSON.parse(localStorage.getItem("users"));
+    for (let user in users){
+        let div1 = document.createElement("div");
+        div1.setAttribute("class","s-12 m-12 l-4 margin-bottom");
+        let div2 = document.createElement("div");
+        div2.setAttribute("class","padding-2x block-bordered border-radius");
+        let icon = document.createElement("i");
+        icon.setAttribute("class","icon-user_male icon2x text-primary margin-bottom-30");
+        let name = document.createElement("h2");
+        name.setAttribute("class","text-thin");
+        name.innerHTML = users[user]["name"] + " "+ users[user]["surname"];
+        let info = document.createElement("p");
+        info.setAttribute("class","margin-bottom-30");
+        info.innerText = "Email: " + users[user]["email"] + "\n"+"Phone: "+users[user]["phoneNumber"];
+        let button = document.createElement("div");
+        button.innerHTML = "EDIT USER";
+        button.setAttribute("class","button border-radius background-primary text-size-12 text-white text-strong");
+        div2.appendChild(icon);
+        div2.appendChild(name);
+        div2.appendChild(info);
+        div2.appendChild(button);
+        div1.appendChild(div2);
+        document.getElementById("staff").appendChild(div1);
+    }
 }
 function createBooks() {
+    div = document.getElementById("label");
+    div.innerHTML = "Books";
     let my_JSON_object = JSON.parse(localStorage.getItem("books"));
     for(let book of my_JSON_object){
         let div1 = document.createElement("div");
